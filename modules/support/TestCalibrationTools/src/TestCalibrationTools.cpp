c608a9a59752575f71874dd5faa2046dc7d41f81
Jake Fountain <jake.fountain@uon.edu.au>
2015-11-18 10:59:21 +1100

    Added test for quat-rot conversion

--- modules/support/TestCalibrationTools/src/TestCalibrationTools.cpp
+++ modules/support/TestCalibrationTools/src/TestCalibrationTools.cpp
@@ -52,74 +52,97 @@ namespace support {
 			float errorThreshold = std::fmax(0.001,config["angle_stddev"].as<float>() + config["disp_stddev"].as<float>());
         	std::cout << "errorThreshold = " << errorThreshold << std::endl;
         	int fails = 0;
-			for(int j = 0; j < config["number_of_trials"].as<int>(); j++){	
-				// simWorldTransform[key] = Transform3D::getRandom(1,0.1);
-				Transform3D X = Transform3D::getRandomU(1,0);//transpose because column major reading
-				Transform3D Y = Transform3D::getRandomU(1,0);
-				
-
-				int N = config["number_of_samples"].as<int>();
-				std::vector<Transform3D> samplesA;
-				std::vector<Transform3D> samplesB;
-				for(int i = 0; i < N; i++){
-					//sample B randomly
-					Transform3D B = Transform3D::getRandomU(1,1);
-					//Noise:
-					Transform3D localNoise = Transform3D::getRandomN(config["angle_stddev"].as<float>() , config["disp_stddev"].as<float>() );
-					// std::cout << "noise = " << arma::vec4(localNoise * arma::vec4({0,0,0,1})).t() << std::endl;
-					Transform3D globalNoise = Transform3D::getRandomN(config["angle_stddev"].as<float>()  ,config["disp_stddev"].as<float>() );
-					// Transform3D globalNoise = Transform3D::getRandomN(0.310524198 ,0.052928682);
-					// Transform3D noise = Transform3D();
+	        if(config["calibration_process_id"].as<int>() == 0){
+				for(int j = 0; j < config["number_of_trials"].as<int>(); j++){	
+					// simWorldTransform[key] = Transform3D::getRandom(1,0.1);
+					Transform3D X = Transform3D::getRandomU(1,1);//transpose because column major reading
+					Transform3D Y = Transform3D::getRandomU(1,0.1);
 					
-			 	 	
-					// std::cout << "slippage[" << rbID << "] = " << Transform3D::norm(slippage[rbID]) << std::endl;
-					// std::cout << "noise[" << rbID << "] = " << Transform3D::norm(noise) << std::endl;
-					// std::cout << "slippage/noise[" << rbID << "] = " << Transform3D::norm(slippage[rbID])/Transform3D::norm(noise) << std::endl;
 
-					Transform3D A =  Y * globalNoise * B * localNoise * X.i();
+					int N = config["number_of_samples"].as<int>();
+					std::vector<Transform3D> samplesA;
+					std::vector<Transform3D> samplesB;
+					for(int i = 0; i < N; i++){
+						//sample B randomly
+						Transform3D B = Transform3D::getRandomU(1,1);
+						//Noise:
+						Transform3D localNoise = Transform3D::getRandomN(config["angle_stddev"].as<float>() , config["disp_stddev"].as<float>() );
+						// std::cout << "noise = " << arma::vec4(localNoise * arma::vec4({0,0,0,1})).t() << std::endl;
+						Transform3D globalNoise = Transform3D::getRandomN(config["angle_stddev"].as<float>()  ,config["disp_stddev"].as<float>() );
+						// Transform3D globalNoise = Transform3D::getRandomN(0.310524198 ,0.052928682);
+						// Transform3D noise = Transform3D();
+						
+				 	 	
+						// std::cout << "slippage[" << rbID << "] = " << Transform3D::norm(slippage[rbID]) << std::endl;
+						// std::cout << "noise[" << rbID << "] = " << Transform3D::norm(noise) << std::endl;
+						// std::cout << "slippage/noise[" << rbID << "] = " << Transform3D::norm(slippage[rbID])/Transform3D::norm(noise) << std::endl;
 
-					samplesA.push_back(A);
-					samplesB.push_back(B);
+						Transform3D A =  Y * globalNoise * B * localNoise * X.i();
+
+						samplesA.push_back(A);
+						samplesB.push_back(B);
+					}
+
+					bool success = true;
+
+					std::pair<Transform3D, Transform3D> result = autocal::CalibrationTools::solveKronecker_Shah2013(samplesA , samplesB, success);
+
+					auto measuredX = result.first;
+					auto measuredY = result.second;
+
+					
+					float errorX = Transform3D::norm(measuredX.i() * X);
+					float errorY = Transform3D::norm(measuredY.i() * Y);
+
+					float totalError = 0;
+					for(int i = 0; i < samplesA.size(); i++){
+						const Transform3D& A = samplesA[i];
+						const Transform3D& B = samplesB[i];
+						Transform3D errorMat = (A * measuredX).i() * (measuredY * B);
+						float error = Transform3D::norm(errorMat);
+						totalError += error;
+						// std::cout << "Error = " << error << " for transform\n" << errorMat << std::endl;
+					}
+
+					if(errorX > errorThreshold || errorY > errorThreshold){
+						fails++;
+						std::cout << "X = \n" << X << std::endl;
+						std::cout << "measuredX = \n" << measuredX << std::endl;
+						std::cout << "Y = \n" << Y << std::endl;
+						std::cout << "measuredY = \n" << measuredY << std::endl;
+
+
+						std::cout << "Transform3D::norm(measuredX .i() *  X) = " << errorX << std::endl;
+						std::cout << "Transform3D::norm(measuredY .i() *  Y) = " << errorY << std::endl;
+						std::cout <<  "total error = " << totalError << std::endl;
+					}
 				}
+			} 
+			else if(config["calibration_process_id"].as<int>() == 1)
+			{
+				for(int j = 0; j < config["number_of_trials"].as<int>(); j++){	
+					UnitQuaternion q = UnitQuaternion::getRandomU(M_PI);
+					Rotation3D R(q);
+					UnitQuaternion q2(R);
+					Rotation3D R2(q2);
 
-				bool success = true;
+					if(arma::norm((q-q2) - arma::vec4({1,0,0,0})) > errorThreshold
+					|| Rotation3D::norm(R.t() * R2) > errorThreshold){
+						std::cout << "q = " << (q).t() << std::endl;
+						std::cout << "q2 = " << (q2).t() << std::endl;
+						std::cout << "q*q2.i() = " << (q-q2).t() << std::endl;
+						std::cout << "R-R2 = " << Rotation3D::norm(R.t() * R2) << std::endl;
 
-				std::pair<Transform3D, Transform3D> result = autocal::CalibrationTools::solveKronecker_Shah2013(samplesA , samplesB, success);
-
-				auto measuredX = result.first;
-				auto measuredY = result.second;
-
-				
-				float errorX = Transform3D::norm(measuredX.i() * X);
-				float errorY = Transform3D::norm(measuredY.i() * Y);
-
-				float totalError = 0;
-				for(int i = 0; i < samplesA.size(); i++){
-					const Transform3D& A = samplesA[i];
-					const Transform3D& B = samplesB[i];
-					Transform3D errorMat = (A * measuredX).i() * (measuredY * B);
-					float error = Transform3D::norm(errorMat);
-					totalError += error;
-					// std::cout << "Error = " << error << " for transform\n" << errorMat << std::endl;
+						fails++;
+					}
 				}
+			} 
+			std::cout << "Total failures: " << fails << " / " << config["number_of_trials"].as<int>() << std::endl;
 
-				if(errorX > errorThreshold || errorY > errorThreshold){
-					fails++;
-					std::cout << "X = \n" << X << std::endl;
-					std::cout << "measuredX = \n" << measuredX << std::endl;
-					std::cout << "Y = \n" << Y << std::endl;
-					std::cout << "measuredY = \n" << measuredY << std::endl;
-
-
-					std::cout << "Transform3D::norm(measuredX .i() *  X) = " << errorX << std::endl;
-					std::cout << "Transform3D::norm(measuredY .i() *  Y) = " << errorY << std::endl;
-					std::cout <<  "total error = " << totalError << std::endl;
-				}
-			}
-			std::cout << "Total failures: " << fails << " / " << config["number_of_trials"].as<int>() << std::endl; 
 
         });
 
     }
+
 }
 }
