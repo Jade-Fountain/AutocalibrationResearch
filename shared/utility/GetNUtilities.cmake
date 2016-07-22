a9054164194147eabbdd9344fad466b8c6495c01
Jake Fountain <jake.fountain@uon.edu.au>
2016-07-22 18:35:58 +1000

    Changed to GetNUtilities

--- shared/utility/GetNUtilities.cmake
+++ shared/utility/GetNUtilities.cmake
@@ -1,9 +1,20 @@
 #GetNUtilities.cmake
+MESSAGE("** Configuring NUtilities...")
+
+SET(NUTILITIES_SRC_FILES "")
 
 # Get source files based on configuration of libraries and project
 IF(${PROJECT_NAME} MATCHES NUbots)
-	MESSAGE("Including NUbots utilities...")
-	FILE(GLOB_RECURSE src "*/**.cpp" "*/**.c" "*/**.h")
+	MESSAGE("** Using NUbots utilities...")
+	FILE(GLOB_RECURSE NUTILITIES_SRC_FILES 
+		"${NUTILITIES_DIR}/*/**.cpp" 
+		"${NUTILITIES_DIR}/*/**.c" 
+		"${NUTILITIES_DIR}/*/**.h")
 ENDIF()
 
-SET(NUTILITIES_SRC_FILES ${src})
+IF(${NUTILITIES_DIR} MATCHES "")
+	message( FATAL_ERROR "NO UTILITIES LOADED - please check conditions for loading utilities" )
+ENDIF()
+
+# MESSAGE(CURRENT_DIRECTORY ${CURRENT_DIRECTORY})
+
