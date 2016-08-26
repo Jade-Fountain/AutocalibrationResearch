28f5afd979a59906b68b18a1e5441cb7a838eb36
Jake Fountain <jake.fountain@uon.edu.au>
2016-08-26 13:35:47 +1000

    Loaded streams into correlation

--- module/fusion/SensorCorrelator/src/SensorCorrelator.cpp
+++ module/fusion/SensorCorrelator/src/SensorCorrelator.cpp
@@ -1,13 +1,13 @@
 #include "SensorCorrelator.h"
 
 #include "message/support/Configuration.h"
-#include "message/input/MotionCapture.h"
 
 namespace module {
 namespace fusion {
 
     using message::support::Configuration;
     using message::input::OpenNIData;
+    using message::input::RigidBodyFrame;
 
     using autocal::MocapStream;
 
@@ -25,17 +25,43 @@ namespace fusion {
 
 		    sensorPlant.addStream(openniStream);
 		    sensorPlant.addStream(mocapStream);
+	       
+	        start_time = std::chrono::system_clock::now();    
         });
 
         on<Every<30, 
         Per<std::chrono::seconds>>,
         Optional<With<OpenNIData>>,
+        Optional<With<RigidBodyFrame>>,
         Single>().then("SensorCorrelator - Sensor Matching Loop",
-    	[this](const std::shared_ptr<const OpenNIData> openniData){
+    	[this](
+    		const std::shared_ptr<const OpenNIData> openniData,
+    		const std::shared_ptr<const RigidBodyFrame> mocapData
+    		){
 
+	        std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();    
+	        double frame_time_since_start = std::chrono::duration_cast<std::chrono::milliseconds>(now-start_time).count() / float(std::milli::den);  
 
+	        autocal::TimeStamp current_timestamp = std::chrono::duration_cast<std::chrono::microseconds>(now-start_time).count();
+    		
+
+    		if(openniData){
+    			for(auto& user : openniData->users){
+    				addData(OPENNI_STREAM, user.second, current_timestamp, user.first);
+    			}
+    		}
+			
+			if(mocapData){
+				addData(MOCAP_STREAM, *mocapData, current_timestamp);
+    		}
 
     	});
+    }
+
+    void SensorCorrelator::addData(std::string stream, const RigidBodyFrame& rigidBodies, autocal::TimeStamp t, int id_offset){
+		for(auto& pose : rigidBodies.poses){
+			sensorPlant.mocapRecording.addMeasurement(stream, t, pose.first, pose.second);
+		}
     }
 }
 }
--- module/fusion/SensorCorrelator/src/SensorCorrelator.h
+++ module/fusion/SensorCorrelator/src/SensorCorrelator.h
@@ -3,6 +3,7 @@
 
 #include <nuclear>
 #include "utility/autocal/SensorPlant.h"
+#include "message/input/MotionCapture.h"
 
 namespace module {
 namespace fusion {
@@ -12,7 +13,10 @@ namespace fusion {
 
 		const std::string OPENNI_STREAM = "openni";
 		const std::string MOCAP_STREAM = "mocap";
+	    
+	    std::chrono::time_point<std::chrono::system_clock> start_time;    
 
+    	void addData(std::string stream, const message::input::RigidBodyFrame& rigidBodies, autocal::TimeStamp t, int id_offset = 0);
     public:
         /// @brief Called by the powerplant to build and setup the SensorCorrelator reactor.
         explicit SensorCorrelator(std::unique_ptr<NUClear::Environment> environment);
